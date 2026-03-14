e5a19f4e7a3a677f61f02a03b1dc1b7e399d0497
2ndgenaccord1984 <jiffysmith2@gmail.com>
2026-03-13 19:24:58 -0700

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,17 +1,10 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
-
-const firebaseConfig = {
+window.FALLON_FIREBASE_CONFIG = {
   apiKey: "AIzaSyDQAtEZ34g_a8l0criXxuPpq81-iw5_uhY",
   authDomain: "turnaround-tracker-28e95.firebaseapp.com",
   databaseURL: "https://turnaround-tracker-28e95-default-rtdb.firebaseio.com",
   projectId: "turnaround-tracker-28e95",
   storageBucket: "turnaround-tracker-28e95.firebasestorage.app",
   messagingSenderId: "612118976397",
-  appId: "1:612118976397:web:f83395c227bd47ca0b8fc4"
+  appId: "1:612118976397:web:f83395c227bd47ca0b8fc4",
+  trackerPath: "fallonSafetyKleenTurnaroundTracker/sharedState"
 };
-
-const app = initializeApp(firebaseConfig);
-const database = getDatabase(app);
-
-export { database };
